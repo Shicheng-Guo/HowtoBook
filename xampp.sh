ccc95f8ce2e3e208aefb266cb9ed5e019c44229c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-07-15 17:42:49 -0700

    Create xampp.sh

---
+++ xampp.sh
@@ -0,0 +1,16 @@
+# 
+
+# stop and restart
+sudo /etc/init.d/apache2 stop
+sudo /etc/init.d/mysql stop
+sudo /etc/init.d/proftpd stop
+sudo /opt/lampp/lampp start
+sudo service vsftpd stop
+sudo /opt/lampp/lampp stop
+
+# start xampp
+sudo /opt/lampp/lampp start
+
+# ifconfig to find IP
+http://132.239.189.199
+
