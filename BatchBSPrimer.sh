c33e9bdbd28fad8fe39655f078259664d41d44ec
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-07-29 17:39:35 -0700

    Update BatchBSPrimer.sh

--- BatchBSPrimer.sh
+++ BatchBSPrimer.sh
@@ -2,4 +2,5 @@ bedtools getfasta -fi ~/oasis/db/hg19_lambda.fa -bed venn.bed -fo esca.fa.bsp.fa
 perl -p -i -e 's/CG/X/ig' esca.fa.bsp.fa
 perl -p -i -e 's/C/T/ig' esca.fa.bsp.fa
 perl -p -i -e 's/X/CG/ig' esca.fa.bsp.fa
+perl -p -i -e 's/Thr/chr/ig' esca.fa.bsp.fa
 # send esca.fa.bsp.fa to http://batchprimer3.bioinformatics.ucdavis.edu/cgi-bin/batchprimer3/batchprimer3.cgi 
