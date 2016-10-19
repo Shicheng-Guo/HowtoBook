6d30a60d6d3d18154180de5904c221eec802d4ce
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-10-19 13:43:28 -0700

    Update Roadmap.sh

---
+++ Roadmap/Roadmap.sh
@@ -0,0 +1,12 @@
+
+cd ~/bin/
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigAverageOverBed  ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigCat ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigCluster ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigCorrelate ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigInfo ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigMerge ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigSummary ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToBedGraph ./
+wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToWig ./
+
