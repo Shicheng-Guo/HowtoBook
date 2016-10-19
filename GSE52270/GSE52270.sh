afa5904da670f955eeab1ba31f0dea5006177c76
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-10-19 12:50:10 -0700

    Update GSE52270.sh

--- GSE52270/GSE52270.sh
+++ GSE52270/GSE52270.sh
@@ -3,5 +3,13 @@ do
 perl GSE52270.pl $i > $i.bedgraph &
 done
 
+for i in `ls *bedgraph`
+do
+sort -k1,1 -k2,2n $i > $i.sort &
+done
 
+for i in `ls *bedgraph`
+do
+bedGraphToBigWig $i.sort ~/oasis/db/hg19/hg19.chrom.sizes $i.sort.hg19.bw &
+done
 
