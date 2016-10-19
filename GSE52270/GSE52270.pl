66357810eee165f011d5ea3b1d422b3ee3021b76
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-10-19 11:12:21 -0700

    Update GSE52270.pl

--- GSE52270/GSE52270.pl
+++ GSE52270/GSE52270.pl
@@ -1,2 +1,15 @@
 #!/usr/bin/perl
 use strict;
+use Cwd;
+chdir getcwd;
+
+my $file=shift @ARGV;
+open F,$file;
+while(<F>){
+chomp;
+my($chr,$pos,$meth,$unmeth)=split /\t/;
+my $start=$pos-2;
+my $end=$pos-1;
+my $ratio=$meth/($meth+$unmeth+0.001);
+print "chr$chr\t$start\t$end\t$ratio\n";
+}
