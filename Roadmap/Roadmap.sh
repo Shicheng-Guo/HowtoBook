1f16c575c8f547b8a707ff8fdbbdacceb6d08039
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-10-20 17:09:22 -0700

    Update Roadmap.sh

--- Roadmap/Roadmap.sh
+++ Roadmap/Roadmap.sh
@@ -10,3 +10,24 @@ wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigSummary ./
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToBedGraph ./
 wget http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/bigWigToWig ./
 
+
+
+# How to get histonoe modification regions
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE56nnn/GSE56712/suppl/GSE56712_RAW.tar
+tar xvf GSE56712_RAW.tar
+rm GSE56712_RAW.tar
+rm *bam
+rm *bigWig
+gunzip *.gz
+for i in H3k27ac H3k27me3 H3k79me2 H3k9me3 H3k4me2
+do
+cat *$i*broadPeak > promter.txt
+awk '{if ($7>5) print $1,$2,$3}' OFS="\t" promter.txt > promter.bed
+sort -k1,1 -k2,2n promter.bed > promterSort.bed
+bedtools merge -i promterSort.bed > Human-hg19-$i-PMID24119843.bed
+done
+rm promter.txt
+rm promter.bed
+rm promterSort.bed
+scp shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/Roadmap/Histone/*SEA* .
+
