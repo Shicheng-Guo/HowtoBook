7232438e299e91a9cdedcffc5cd862e82792be6c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-10-22 22:55:40 -0700

    Create TSCC2GenomeMiner.sh

---
+++ TSCC2GenomeMiner.sh
@@ -0,0 +1,6 @@
+
+# move bam2hapinfo pipeline from TSCC to Genome-miner
+
+scp /home/shg047/oasis/db/mm9/mm9.chrom.sizes shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/db/mm9
+scp /home/shg047/bin/mergedBam2hapInfoV2.pl shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/bin
+scp /home/shg047/oasis/db/mm9/MM9.CpG.positions.txt shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/db/mm9
