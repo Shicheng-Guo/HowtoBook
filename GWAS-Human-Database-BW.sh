8e3d973165efa45e01add57b9be17501b221015a
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-02 13:30:04 -0700

    Update GWAS-Human-Database-BW.sh

--- GWAS-Human-Database-BW.sh
+++ GWAS-Human-Database-BW.sh
@@ -9,3 +9,9 @@
 
 # PBMC methylome in Esteller2012PNAS Age project
 /media/Home_Raid1/shg047/NAS3/Esteller2012PNAS/bw
+
+
+
+# copy bw to 132.239.189.199 (meangenemachine)
+
+scp /media/Home_Raid1/shg047/NAS3/Roadmap/*bw shicheng@132.239.189.199:/home/ucsd002/wig/wgbs/
