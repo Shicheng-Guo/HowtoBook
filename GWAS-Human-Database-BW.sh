595d196e805113365421a828cef5285ddcada67f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-02 13:51:09 -0700

    Update GWAS-Human-Database-BW.sh

--- GWAS-Human-Database-BW.sh
+++ GWAS-Human-Database-BW.sh
@@ -1,17 +1,23 @@
 # Roadmap Epigenome
 /media/Home_Raid1/shg047/NAS3/Roadmap
+sudo scp shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/Roadmap/wig/*bw ./
 
 # Holger2016
 /home/shg047/oasis/Holger2016/bw
+sudo scp shg047@genome-miner.ucsd.edu:/home/shg047/oasis/Holger2016/bw/*bw ./
 
 # Xliu2014
 /home/shg047/oasis/Xliu2014/bw
+sudo scp shg047@genome-miner.ucsd.edu:/home/shg047/oasis/Xliu2014/bw/*bw ./
 
 # PBMC methylome in Esteller2012PNAS Age project
 /media/Home_Raid1/shg047/NAS3/Esteller2012PNAS/bw
-
+sudo scp shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/Esteller2012PNAS/bw/*bw ./
 
 
 # copy bw to 132.239.189.199 (meangenemachine)
-
-scp /media/Home_Raid1/shg047/NAS3/Roadmap/*bw shicheng@132.239.189.199:/home/ucsd002/wig/wgbs/
+cd /home/ucsd002/wig/wgbs
+sudo scp shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/Roadmap/wig/*bw ./
+sudo scp shg047@genome-miner.ucsd.edu:/home/shg047/oasis/Holger2016/bw/*bw ./
+sudo scp shg047@genome-miner.ucsd.edu:/home/shg047/oasis/Xliu2014/bw/*bw ./
+sudo scp shg047@genome-miner.ucsd.edu:/media/Home_Raid1/shg047/NAS3/Esteller2012PNAS/bw/*bw ./
