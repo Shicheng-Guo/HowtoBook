f2581935c91c57b14053ff9fee901bc7daf8b373
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-03 16:12:39 -0700

    Update GSE39775.sh

--- LaSalle2012PNAS/GSE39775.sh
+++ LaSalle2012PNAS/GSE39775.sh
@@ -4,12 +4,21 @@ tar xvf GSE39775_RAW.tar
 tar xzvf *.tar.gz
 gunzip *.gz
 
-cat GSM1083881.*BED > GSM1083881.Placenta.Placenta3.BED
-cat GSM1083880.*BED > GSM1083880.Placenta.Placenta3.BED
 
-cat placenta_rep2_MethylC-seq_chr*.BED > GSM978964.Placenta.Placenta1-1.BED
-cat placenta_rep1_MethylC-seq_chr*.BED > GSM1083881.Placenta.Placenta1-2.BED
-GSM978965
-GSM1083880_placenta2_MethylC-seq_chr20.BED
+cat placenta_rep1_MethylC-seq_chr*.BED > GSM978964.Placenta.Placenta1-1.BED
+cat placenta_rep2_MethylC-seq_chr*.BED > GSM978965.Placenta.Placenta1-2.BED
+cat GSM1083880_*BED > GSM1083880.Placenta.Placenta2.BED
+cat GSM1083881_*BED > GSM1083881.Placenta.Placenta3.BED
+cat kidney_MethylC-seq_chr*.BED > GSM978966.Kidney.BED
+cat cerebellum_MethylC-seq_chr*.BED > GSM978968.Brain.BED
+cat NK_MethylC*.BED > GSM978967.NK.BED
+
+rm *chr*
+
+for i in `ls *BED`
+do
+awk '{print $1,$2,$3,$4}' $i | sort -k1,1 -k2,2n > $i.sort
+done
+
 # hg18 to hg19
 
