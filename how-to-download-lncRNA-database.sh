b4adf1a97cae92176f40e064adef771dfb6c1576
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-16 09:59:18 -0800

    Update how-to-download-lncRNA-database.sh

--- how-to-download-lncRNA-database.sh
+++ how-to-download-lncRNA-database.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+# where to download lncRNA databse
 wget ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.long_noncoding_RNAs.gtf.gz
 tar xzvf gencode.v19.long_noncoding_RNAs.gtf.gz
 awk 'NR>5 {print $1,$4,$5,$10}' gencode.v19.long_noncoding_RNAs.gtf > lncRNA.hg19.bed
