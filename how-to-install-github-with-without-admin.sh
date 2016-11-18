1ab70949c7d3b83d6f3b27cff96bba621b3b2adf
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-17 16:54:17 -0800

    Update how-to-install-github-with-without-admin.sh

--- how-to-install-github-with-without-admin.sh
+++ how-to-install-github-with-without-admin.sh
@@ -1,3 +1,4 @@
+
 # How to install git with adminstration 
 sudo apt-get update
 sudo apt-get install git
@@ -6,13 +7,15 @@ git config --global user.email "shicheng.guo@hotmail.com"
 git config --list
 git commit --amend --reset-author
 
-# How to install git without admin
-cd
-mkdir git
-cd git
+# How to install git without admin (TSCC in UCSD)
+cd software
 wget https://www.kernel.org/pub/software/scm/git/git-2.10.2.tar.xz
 tar xf xzvf git-2.10.2.tar.xz
 cd git-2.10.2
 ./configure --prefix="$HOME/git"
 make install
 git config --list
+
+# Go to home and bulid the databse for git
+cd
+mkidr git
