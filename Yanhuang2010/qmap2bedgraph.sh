ac5d64b47bc35725c8c2e3328a586026afe1c340
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-19 16:28:12 -0800

    Update qmap2bedgraph.sh

--- Yanhuang2010/qmap2bedgraph.sh
+++ Yanhuang2010/qmap2bedgraph.sh
@@ -5,6 +5,8 @@
 # Update: 12/19/2016
 # interesting: sort hg18 bedgraph will be non-sorted after liftOver to hg19 and hg38
 
+# qmap to bedgraph, liftOver hg18 to hg19 and hg38, finally sort the bedgraph 
+
  for i in `ls *.txt`
  do
  echo $i
@@ -15,11 +17,14 @@
  sort -k1,1 -k2,2n $i.bedgraph.hg19 > $i.bedgraph.hg19.sort
  sort -k1,1 -k2,2n $i.bedgraph.hg38 > $i.bedgraph.hg38.sort
  done
+ 
+ # merge bedgraph 
 
  cat *hg18.sort > GSE17972.YanHuang.hg18.bedgraph 
  cat *hg19.sort > GSE17972.YanHuang.hg19.bedgraph 
  cat *hg38.sort > GSE17972.YanHuang.hg38.bedgraph 
- 
+
+ # bedgraph to bigwig 
  for i in `ls *.bedgraph`
  do
  echo $i
@@ -27,4 +32,3 @@
  bedGraphToBigWig $i ~/work/db/hg19/hg19.chrom.sizes $i.bw
  bedGraphToBigWig $i ~/work/db/hg38/hg38.chrom.sizes $i.bw
  done
-
