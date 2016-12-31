4c5b1b836d5b003b0793c7aa8f36e269dd591260
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-30 22:45:34 -0800

    Update rmsk.sh

--- RepeatMaskMethylome/rmsk.sh
+++ RepeatMaskMethylome/rmsk.sh
@@ -52,3 +52,37 @@ tapply(input.long$value,input.long$group,function(x) median(x))
 
 
 
+# Then how about CpG island regions
+
+# 2016-12-30
+cd /media/Home_Raid1/shg047/work/Roadmap/bw
+for i in GSM1010981_UCSD.Adrenal_Gland.Bisulfite-Seq.STL003.wig.gz.bw GSM983649_UCSD.Esophagus.Bisulfite-Seq.STL003.wig.gz.bw
+do
+bigWigAverageOverBed $i /media/Home_Raid1/shg047/work/db/hg19/CpGI.hg19.bed4 $i.tab
+done
+cd /media/Home_Raid1/shg047/work/Chen2016CellResearch/bw
+for i in `ls *bw`
+do
+bigWigAverageOverBed $i /media/Home_Raid1/shg047/work/db/hg19/CpGI.hg19.bed4 $i.tab
+mv $i.tab /media/Home_Raid1/shg047/work/Roadmap/bw
+done
+cd /media/Home_Raid1/shg047/work/Roadmap/bw
+perl ~/bin/bigWigAverageOverBed2Matrix.pl > CpGI.txt
+
+data<-read.table("CpGI.txt")
+info<-read.table("/media/Home_Raid1/shg047/work/db/hg19/CpGI.hg19.bed4")
+group<-unlist(lapply(info[,4],function(x) unlist(strsplit(as.character(x),":"))[3]))
+table(group)
+input<-data.frame(data,group)
+head(input)
+library("reshape2")
+colnames(input)<-c(c("Kidney","Esophagus"),"group")
+input.long<-melt(input, id.vars=c("group"))
+library(ggplot2)
+png("rmsk.methylation.png")
+ggplot(aes(y = value, x = group, fill = variable, dodge=variable), data = input.long) + geom_boxplot(outlier.shape =NA,outlier.colour="white")+ coord_flip()
+dev.off()
+
+
+
+
