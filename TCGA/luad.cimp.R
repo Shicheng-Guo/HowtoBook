e543b97cf3d10470ee9e86e68dab1ff6eb6f9333
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-01-04 15:06:26 -0800

    Create luad.cimp.R

---
+++ TCGA/luad.cimp.R
@@ -0,0 +1,59 @@
+#!/usr/bin/R
+# identify lung CIMP (paired cancer and normal) and validate it in LUAD (all the cancer sample)
+# Contact: Shicheng Guo
+# Version 1.3
+# Update: 1/4/2017
+
+
+# Download all the HM450K file and save them in the working directory
+file=list.files(pattern="jhu-usc.edu_LUAD")
+idv<-unique(as.array(str_extract(file,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*")))
+idv1<-as.array(str_extract(file,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+pairidv<-c()
+for (i in 1:length(idv)){
+  t1<-paste(idv[i],"-01",sep="") 
+  t2<-paste(idv[i],"-11",sep="")
+  if(all(any(grepl(t1,file)),any(grepl(t2,file)))){
+    pairidv<-c(pairidv,t1,t2)
+  }
+}
+
+# calculate the sample size for each cancer type
+pairfile<-file[sapply(pairidv,function(x){x<-grep(x,file);x[length(x)]})]
+samplesize(pairfile)
+allfile<-file[sapply(idv1,function(x){x<-grep(x,file);x[length(x)]})]
+samplesize(allfile)
+
+
+data1<-c()
+for(i in 1:length(pairfile)){
+  tmp<-read.table(pairfile[i],head=T,sep="\t",as.is=F,skip=1)  # tmp<-read.table(file[i],head=T,sep="\t",as.is=F)
+  data1<-cbind(data1,tmp[,2])
+  print(c(i,pairfile[i]))
+  rownames(data1)<-tmp[,1]
+}
+
+data2<-c()
+for(i in 1:length(allfile)){
+  tmp<-read.table(allfile[i],head=T,sep="\t",as.is=F,skip=1)  # tmp<-read.table(file[i],head=T,sep="\t",as.is=F)
+  data2<-cbind(data2,tmp[,2])
+  rownames(data2)<-tmp[,1]
+  print(c(i,allfile[i]))
+}
+
+# CIMP: Q0>0.3 and Q50>0.6
+cimp.luad<-CIMP(data1)
+positive.ratio<-lappy(data2[match(names(cimp.luad),rownames(data2)),],2,function(x) sum(x>0.3)/length(x))
+
+## Functions related to CIMP
+CIMP<-function(dataframe){
+Q<-apply(dataframe,1,function(x) quantile(x,c(0,0.5),na.rm = T))
+names(Q)=rownames(dataframe)
+QP<-which(apply(Q,2,function(x) x[1]>0.5))
+return(QP)
+}
+
+samplesize<-function(file){
+  return(table(unlist(lapply(file,function(x) unlist(strsplit(x,"[_.]"))[3]))))
+}
+
