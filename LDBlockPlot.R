446163c8b1ecad825ec3620fd62662f3a0ea3838
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-03-22 01:24:32 -0700

    Create LDBlockPlot.R

---
+++ LDBlockPlot.R
@@ -0,0 +1,14 @@
+# LD block with R
+install.packages("gaston")
+library("gaston")
+data(AGT)
+x <- as.bed.matrix(AGT.gen, AGT.fam, AGT.bim)
+# Compute LD
+ld.x <- LD(x, c(1,ncol(x)))
+# Plot a tiny part of the LD matrix
+LD.plot( ld.x[1:20,1:20], snp.positions = x@snps$pos[1:20] )
+# Customize the plot
+LD.plot( ld.x[1:20,1:20], snp.positions = x@snps$pos[1:20], 
+         graphical.par = list(cex =1.5, bg = "gray"), 
+         polygon.par = list(border = NA), write.ld = NULL )
+
