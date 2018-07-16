b619b3eadbc6d635ec3d12565bcc5febeabca62c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-07-16 11:35:43 -0500

    Update readme.sh

--- R/readme.sh
+++ R/readme.sh
@@ -5,3 +5,7 @@ sudo apt-get install texlive
 sudo apt-get install texlive-fonts-extra
 
 https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest
+
+# Resizing Azure VM OS or Data Disk
+https://blogs.msdn.microsoft.com/madan/2015/11/02/resizing-azure-vm-os-or-data-disk/
+
