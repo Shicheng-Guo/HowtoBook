c74f48f7f47171e0265bb0fb3940760876d385da
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-07-18 10:37:09 -0500

    Update mega2.install.sh

--- mega2.install.sh
+++ mega2.install.sh
@@ -1,3 +1,4 @@
-Optional: Permanently enable scl toolchain by putting this in your .bashrc (warning: don't try to use the scl enable devtoolset-2 bash command from before in your .bashrc. This spawns a new bash shell, and if that's in your .bashrc, it creates a new shell, which loads your .bashrc, which creates a new shell, etc.)
-source /opt/rh/devtoolset-2/enable
-may have worked also
+# Optional: Permanently enable scl toolchain by putting this in your .bashrc (warning: don't try to use the scl enable devtoolset-2 bash command from before in your .bashrc. This spawns a new bash shell, and if that's in your .bashrc, it creates a new shell, which loads your .bashrc, which creates a new shell, etc.)
+# source /opt/rh/devtoolset-2/enable
+# may have worked also
+scl enable devtoolset-2 bash
