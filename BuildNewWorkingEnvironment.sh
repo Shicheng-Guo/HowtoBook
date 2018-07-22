2c2d5f87150b3dd7c129ea4a386ac47056ce4a33
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-07-22 00:50:40 -0500

    Update BuildNewWorkingEnvironment.sh

--- BuildNewWorkingEnvironment.sh
+++ BuildNewWorkingEnvironment.sh
@@ -9,4 +9,28 @@
 
 # install Python
 
+1. miniconda3
+2. 
+
+
+# build C tools
+
+1. samtools
+2. bedtools
+3. vcftools
+4. gatk
+5. fastqc
+6. bismark
+7. primer3
+8. trimGalore
+9. bowtie2
+10. eagle
+11. beagle
+12. fastPhase
+13. cutadapt
+14. fs
+15. tabix
+16. annovar
+
+
 
