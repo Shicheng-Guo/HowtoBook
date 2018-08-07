19815329c69437826a72aea297ab6ecc5d61372c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-07 10:01:30 -0500

    Create wigdownload.sh

---
+++ blueprint/wigdownload.sh
@@ -0,0 +1,6 @@
+http://dcc.blueprint-epigenome.eu/#/experiments/ERX1588239
+
+http://ftp.ebi.ac.uk/pub/databases/blueprint/data/homo_sapiens/GRCh38/bone_marrow/S01GRF/Acute_Lymphocytic_Leukemia/Bisulfite-Seq/CNAG/S01GRFA1.CPG_methylation_calls.bs_call.GRCh38.20160531.bw
+
+
+wget -e robots=off -nH -r -nd --reject="index.html*" -l 6 -A "*CPG_methylation_calls.bs_call.GRCh38.20160531.bw" --no-parent http://ftp.ebi.ac.uk/pub/databases/blueprint/data/homo_sapiens/GRCh38/
