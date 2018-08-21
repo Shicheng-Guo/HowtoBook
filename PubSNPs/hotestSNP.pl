48c9bd798d972e6cedaa1f2810fd8dbbf5a37c40
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-21 17:36:27 -0500

    Create hotestSNP.pl

---
+++ PubSNPs/hotestSNP.pl
@@ -0,0 +1,18 @@
+use strict;
+use Cwd;
+chdir getcwd;
+my $file=shift @ARGV;
+my %snp;
+open F,$file || die "cannot open $file\n";
+open OUT,">SNP.count.txt";
+my $j;
+while(<F>){
+if(/(rs\d+)/){
+$snp{$1}++;
+}
+}
+foreach my $snp(sort keys %snp){
+print OUT "$snp\t$snp{$snp}\n";
+}
+close F;
+close OUT;
