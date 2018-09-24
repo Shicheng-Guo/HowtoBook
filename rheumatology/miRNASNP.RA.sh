d1e0d36817ab686c37f2f2422bc83276243a3b0d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-09-24 02:57:59 -0500

    Update miRNASNP.RA.sh

--- rheumatology/miRNASNP.RA.sh
+++ rheumatology/miRNASNP.RA.sh
@@ -1,5 +1,9 @@
 
 ```
 cd /gpfs/home/guosa/hpc/rheumatology/RA/miRNASNP
-
+R
+data<-read.table("immune_disease.txt",head=T,sep="\t")
+data<-read.table("RA.GWAS.SNP.txt",head=F,sep="\t")
+grep("chr",data$V4)
+snp150<-read.table("~/hpc/db/hg19/snp150.hg19.txt")
 ```
