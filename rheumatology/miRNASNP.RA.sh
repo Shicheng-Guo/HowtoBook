be7b0a3823a3a1c6d27d87dc5246bd14a8255322
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-09-24 03:06:13 -0500

    Update miRNASNP.RA.sh

--- rheumatology/miRNASNP.RA.sh
+++ rheumatology/miRNASNP.RA.sh
@@ -6,4 +6,10 @@ data<-read.table("immune_disease.txt",head=T,sep="\t")
 data<-read.table("RA.GWAS.SNP.txt",head=F,sep="\t")
 grep("chr",data$V4)
 snp150<-read.table("~/hpc/db/hg19/snp150.hg19.txt")
+
+rlt<-unique(data.frame(paste("chr",data$V2,sep=""),data$V3,data$V3+1))
+write.table(rlt,file="RA.GWAS.SNP.hg19.bed",sep="\t",col.names=F,row.names=F,quote=F)
+
+bedtools intersect -wao -a RA.GWAS.SNP.hg19.bed -b ~/hpc/db/hg19/snp150.hg19.txt
 ```
+
