9a6c30badc7a1fd5273bba8dd23c114cb48ba169
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-11-07 15:24:56 -0600

    Update bed2avinput.pl

--- ANNOVAR/bed2avinput.pl
+++ ANNOVAR/bed2avinput.pl
@@ -5,15 +5,15 @@ use POSIX;
 my $dir = getcwd;
 chdir $dir;
 
-open F,"/gpfs/home/guosa/hpc/db/hg19/cpgSNP.hg19.bed";
+open F,"/home/guosa/hpc/db/hg19/allSNP150.hg19.bed";
 open OUT,">cpgSNP.hg19.bed.avinput";
 while(<F>){
-        my ($chr,$start,$end,$rs,$ref,$alt,$context)=split/\s+/;
+        my ($chr,$start,$end,$rs,$chain,$ref,$alt)=split/\s+/;
         my @allele=split/\//,$alt;
         my (undef,$newchr)=split/chr/,$chr;
         foreach my $allele(@allele){
                 next if $allele eq $ref;
-                print OUT "$newchr\t$start\t$end\t$rs\t$ref\t$allele\t$context\n";
+                print OUT "$newchr\t$start\t$end\t$ref\t$allele\t$rs\n";
         }
 }
 close OUT;
