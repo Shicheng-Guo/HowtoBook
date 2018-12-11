1188dff0ef0ef1d644af034cd1b5bf8fa2f6dc84
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-12-11 16:23:59 -0600

    Update annovar.hg38.sh

--- 1000Genome/annovar.hg38.sh
+++ 1000Genome/annovar.hg38.sh
@@ -17,9 +17,33 @@ echo vcftools --gzvcf ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.si
 qsub chr$i.job
 done
 
+# download reference database for annnovar hg38
 cd ~/hpc/tools/annovar/
 annotate_variation.pl -buildver hg38 -downdb -webfrom annovar refGene humandb/
-
+annotate_variation.pl -downdb -webfrom annovar exac03 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar esp6500siv2 humandb -buildver hg38 &
+annotate_variation.pl -downdb -webfrom annovar esp6500siv2_all humandb -buildver hg38 &
+annotate_variation.pl -downdb -webfrom annovar gnomad_exome humandb -buildver hg38 &
+annotate_variation.pl -downdb -webfrom annovar 1000g2015aug humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar kaviar_20150923 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar hrcr1 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar cg69 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar gnomad_genome humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar dbnsfp30a humandb -buildver hg38 &
+annotate_variation.pl -downdb -webfrom annovar esp6500siv2 humandb -buildver hg38 &
+annotate_variation.pl -downdb esp6500siv2 humandb -buildver hg38 &
+annotate_variation.pl -downdb -webfrom annovar gerp++ humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar cadd humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar cadd13 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar fathmm humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar eigen humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar gwava humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar dbscsnv11 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar spidex humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar clinvar_20160302 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar cosmic70 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar icgc21 humandb -buildver hg38  &
+annotate_variation.pl -downdb -webfrom annovar nci60 humandb -buildver hg38  &
 
 
 
