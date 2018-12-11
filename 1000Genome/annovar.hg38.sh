44538d162d1df5ea658a5aa6e9e167f539b3ddc4
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-12-11 17:29:39 -0600

    Update annovar.hg38.sh

--- 1000Genome/annovar.hg38.sh
+++ 1000Genome/annovar.hg38.sh
@@ -43,4 +43,17 @@ annotate_variation.pl -downdb -webfrom annovar icgc21 humandb -buildver hg38  &
 annotate_variation.pl -downdb -webfrom annovar nci60 humandb -buildver hg38  &
 
 
-
+# annotation with ANNOVAR from vcf to excel for each chrosome.
+cd /gpfs/home/guosa/hpc/db/hg38/1000genome
+for i in {1..22} X Y
+do
+echo \#PBS -N chr$i  > chr$i.job
+echo \#PBS -l nodes=1:ppn=1 >> chr$i.job
+echo cd $(pwd) >> chr$i.job
+echo vcftools --gzvcf ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz --chr $i --recode --out ./annovar/chr$i >>chr$i.job
+echo convert2annovar.pl -format vcf4 -allsample -withfreq -includeinfo chr$i.recode.vcf  \> ../annovar/chr$i.vcf.avinput >> chr$i.job
+echo table_annovar.pl ../annovar/chr$i.vcf.avinput ~/hpc/tools/annovar/humandb/ --thread 4 -buildver hg38 --csvout -out ../annovar/chr$i.hg38 -remove -protocol refGene,dbnsfp35c,gwasCatalog,wgRna -operation gx,f,r,r -nastring . -otherinfo  -polish -xref ~/hpc/tools/annovar/humandb/gene_fullxref.txt >> chr$i.job
+qsub chr$i.job
+done
+rm *.e*
+rm *.o*
