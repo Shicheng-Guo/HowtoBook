fb258df0728110b7f51f13dcb5ccb3042fa61fc3
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-12-11 14:45:16 -0600

    Create annovar.sh

---
+++ 1000Genome/annovar.sh
@@ -0,0 +1,8 @@
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz.tbi
+gunzip ALL.wgs.phase3_shapeit2_mvncall_integrated_v5b.20130502.sites.vcf.gz
+
+
+
+
+
