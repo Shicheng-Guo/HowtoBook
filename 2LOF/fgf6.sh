44c2bf87ceb6404b30dcadef92c6f19f71debd94
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-12-19 21:44:30 -0600

    Update fgf6.sh

--- 2LOF/fgf6.sh
+++ 2LOF/fgf6.sh
@@ -1,5 +1,6 @@
 
 ```
 bcftools view -i 'R2>0.5' chr12.dose.vcf.gz -Oz -o chr12.dose.filter.vcf.gz
-
+zcat chr12.dose.filter.vcf.gz | awk '{print $1,$2,$3,$4,$5}' OFS="\t" | grep -v '#' > chr12.vcf.avinput
+table_annovar.pl chr12.vcf.avinput /gpfs/home/guosa/hpc/tools/annovar/humandb/ --thread 1 -buildver hg19 --csvout -out chr12 -remove -protocol refGene,dbnsfp35c,gwasCatalog,wgRna -operation gx,f,r,r -nastring . -otherinfo -polish -xref /gpfs/home/guosa/hpc/tools/annovar/humandb/gene_fullxref.txt
 ```
