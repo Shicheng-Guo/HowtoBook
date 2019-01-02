504dc8a8b771de3bd8ed864aff2e309688cbd87f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-01-01 20:31:20 -0600

    Update qqplot.R

--- SSc/IFN/qqplot.R
+++ SSc/IFN/qqplot.R
@@ -1,6 +1,8 @@
 # http://review.frontiersin.org/review/433093/18/232809/#tab/History
 # Shicheng Guo
-# Define the function
 install.packages("Haplin")
 library("Haplin")
-pQQ(pvalues, nlabs =2000, conf = 0.95) 
+cd4<-read.table("CD4.txt",head=T)
+pQQ(cd4[,2], nlabs =nrow(cd4), conf = 0.95) 
+cd8<-read.table("CD8.txt",head=T)
+pQQ(cd8[,2], nlabs =nrow(cd8), conf = 0.95) 
