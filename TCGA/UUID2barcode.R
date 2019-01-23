a65394347e04fc416d503e0b03e7737f53dc1ab3
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-01-22 22:20:34 -0600

    Create UUID2barcode.R

---
+++ TCGA/UUID2barcode.R
@@ -0,0 +1,10 @@
+manifest="gdc_manifest.2019-01-20.txt"
+x=read.table(manifest,header = T)
+manifest_length= nrow(x)
+id= toString(sprintf('"%s"', x$id))
+Part1= '{"filters":{"op":"in","content":{"field":"files.file_id","value":[ '
+Part2= '] }},"format":"TSV","fields":"file_id,file_name,cases.submitter_id,cases.case_id,data_category,data_type,cases.samples.tumor_descriptor,cases.samples.tissue_type,cases.samples.sample_type,cases.samples.submitter_id,cases.samples.sample_id,cases.samples.portions.analytes.aliquots.aliquot_id,cases.samples.portions.analytes.aliquots.submitter_id","size":'
+Part3= paste0("\"",manifest_length, "\"", "}")
+Sentence= paste(Part1,id,Part2,Part3, collapse=" ")
+write.table(Sentence,"Payload.txt",quote=F,col.names=F,row.names=F)
+system("curl --request POST --header \"Content-Type: application/json\" --data @Payload.txt \"https://api.gdc.cancer.gov/files\" > barcode.txt")
