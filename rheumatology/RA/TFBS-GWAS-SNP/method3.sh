d9536acd8db87f578543fdf89be393dc61e85078
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-09 14:16:18 -0600

    Create method3.sh

---
+++ rheumatology/RA/TFBS-GWAS-SNP/method3.sh
@@ -0,0 +1,1 @@
+bedtools intersect -a RA-GWAS-Functional-SNPs-Final.186.Snp.20190208.bed -b GWAS-Meta-128-SNPs.20190208.bed > RA-GWAS-Functional-SNPs-Final.186.Snp.GWAS-Meta-129.overlap.20190208.bed
