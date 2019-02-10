fecea641ddf9db3a5e5a0fe787c92496aac1b056
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-09 20:31:48 -0600

    Update GSE103909.R

--- CHOL/GSE103909.R
+++ CHOL/GSE103909.R
@@ -1,15 +1,16 @@
-wget -r -l 1 -nd -e robots=off --reject jpg,html ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE103nnn/GSE103909/suppl/
+setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/chol")
+
+# wget -r -l 1 -nd -e robots=off --reject jpg,html ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE103nnn/GSE103909/suppl/
 
 library("GEOquery")
 GSE103909 <- getGEO("GSE103909")
 data <- as.data.frame(exprs(GSE103909[[1]]))
 phen <- pData(phenoData(GSE103909[[1]]))
-
-phen1<-sapply(strsplit(as.character(phen$characteristics_ch1.7),"[:]"),function(x) as.numeric(unlist(x)[2]))  # status 1:control, 2:scz
-phen1[phen1==1]<-"Normal"
-phen1[phen1==2]<-"schizophrenia"
-phen2<-sapply(strsplit(as.character(phen$characteristics_ch1),"[:]"),function(x) (unlist(x)[2]))  # gender
-
-data1=na.omit(data)
-PCAPlot(t(data1),phen1,output="GSE41169.scz.normal.pdf",multifigure=T)  # status
-PCAPlot(t(data1),phen2,output="GSE41169.gender.pdf",multifigure=T)  # gender
+phe<-data.frame(rownames(phen),phen$title,phen$`tissue:ch1`)
+cas<-grep("Tumor",phe[,2])
+con<-grep("_Normal",phe[,2])
+pvalue<-apply(data,1,function(x) t.test(x[cas],x[con],paired=T)$p.value)
+output<-data.frame(data,pvalue)
+output<-subset(output,pvalue<0.0005)
+dim(output)
+write.table(output,file="CCA.lncRNA.diff.txt",sep="\t",quote=F,col.names = NA,row.names = T)
