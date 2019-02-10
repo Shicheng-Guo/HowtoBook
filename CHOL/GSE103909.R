f1c93bc68289aee92ad39cddb21a05dd9a65109b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-09 21:01:45 -0600

    Update GSE103909.R

--- CHOL/GSE103909.R
+++ CHOL/GSE103909.R
@@ -1,7 +1,5 @@
 setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/chol")
-
 # wget -r -l 1 -nd -e robots=off --reject jpg,html ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE103nnn/GSE103909/suppl/
-
 library("GEOquery")
 GSE103909 <- getGEO("GSE103909")
 data <- as.data.frame(exprs(GSE103909[[1]]))
@@ -10,7 +8,9 @@ phe<-data.frame(rownames(phen),phen$title,phen$`tissue:ch1`)
 cas<-grep("Tumor",phe[,2])
 con<-grep("_Normal",phe[,2])
 pvalue<-apply(data,1,function(x) t.test(x[cas],x[con],paired=T)$p.value)
-output<-data.frame(data,pvalue)
+tvalue<-apply(data,1,function(x) t.test(x[cas],x[con],paired=T)$statistic)
+
+output<-data.frame(data,tvalue,pvalue)
 output<-subset(output,pvalue<0.0005)
 dim(output)
 write.table(output,file="CCA.lncRNA.diff.txt",sep="\t",quote=F,col.names = NA,row.names = T)
