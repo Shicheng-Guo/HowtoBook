3ccec30791e359ccfced617f3892fe1c22540c79
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-11 20:18:39 -0600

    Create targetScan.sh

---
+++ rheumatology/RA/miRNASNP/targetScan.sh
@@ -0,0 +1,3 @@
+
+wget http://www.targetscan.org/vert_72/vert_72_data_download/Predicted_Target_Locations.default_predictions.hg19.bed.zip
+unzip Predicted_Target_Locations.default_predictions.hg19.bed.zip
