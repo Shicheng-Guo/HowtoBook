f407726207d7cd6dd1522f34ef5d1874f28ff070
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-20 03:40:06 -0600

    Update MCRI-GH-ASA-2019.sh

--- rheumatology/RA/ASA/MCRI-GH-ASA-2019.sh
+++ rheumatology/RA/ASA/MCRI-GH-ASA-2019.sh
@@ -57,9 +57,31 @@ bedtools intersect -v -a 63.miRNA.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$
 wc -l ASA-GH-Guo.A1.bed
 cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
 
+bedtools intersect -v -a 47_NG2012.SupplementaryTable5.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > ASA-GH-Guo.A1.bed
+wc -l ASA-GH-Guo.A1.bed
+cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
+
+bedtools intersect -v -a 145.UTR3miRNAsNP.EAS.MAF0.01.VIP.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > ASA-GH-Guo.A1.bed
+wc -l ASA-GH-Guo.A1.bed
+cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
+
+bedtools intersect -v -a HLA-TagSnp.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > ASA-GH-Guo.A1.bed
+wc -l ASA-GH-Guo.A1.bed
+cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
+
+bedtools intersect -v -a 1345.UTR3miRNAsNP.EAS.MAF0.01.EpiGeneC.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > ASA-GH-Guo.A1.bed
+wc -l ASA-GH-Guo.A1.bed
+cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
+
+bedtools intersect -v -a miRNASeedSNP.hg19.bed -b ASA.hg19.bed | awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > ASA-GH-Guo.A1.bed
+wc -l ASA-GH-Guo.A1.bed
+cat ASA-GH-Guo.A1.bed >> ASA.hg19.bed
+
+
 grep -v 'CHROM'  ASA.hg19.bed | sort -u > ASA.hg19.P2.bed
 wc -l ASA.hg19.P2.bed
 awk '{print "chr"$2,$3-1,$3,$4}' OFS="\t" ASACHIA_rsID.txt | grep -v CHROM | sort -u > ASA.hg19.P1.bed
 wc -l ASA.hg19.P1.bed
 bedtools intersect -v -a ASA.hg19.P2.bed  -b ASA.hg19.P1.bed| awk '{print $1,$2,$3,$4}' OFS="\t" | sort -u > MRCI.hg19.bed
 wc -l MRCI.hg19.bed
+
