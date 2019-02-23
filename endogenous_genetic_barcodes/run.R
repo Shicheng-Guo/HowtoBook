928fd6a472158ba9c2dac6cd4d17328fb561c9c1
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-02-23 16:23:42 -0600

    Update run.R

--- endogenous_genetic_barcodes/run.R
+++ endogenous_genetic_barcodes/run.R
@@ -19,3 +19,57 @@ write.table(rlt,file="highDiversityHaplotype.txt",sep="\t",quote=F,col.names = F
 awk '{print "chr"$1,$2,$3,$4}' OFS="\t" highDiversityHaplotype.txt >  highDiversityHaplotype.hg19.bed
 /gpfs/home/guosa/hpc/db/hg19/1000Genome/highDiversityHaplotype.hg19.bed
 bedtools intersect -wao -a highDiversityHaplotype.txt  -b 68194.GWASCatlog.hg19.bed
+
+
+##################################################################################
+
+setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/db/hg19/1000Genome")
+
+d1<-read.table("chr1.CHB.hap.txt")
+d2<-read.table("chr1.CEU.hap.txt")
+d3<-read.table("chr1.YRI.hap.txt")
+
+png("hapcount500.png")
+par(mfrow=c(3,1))
+plot(d1[,1],type="l",col="red",ylab="# haplotype",main="CHB-N=103",xlab="chr1-500bp-window")
+plot(d2[,1],type="l",col="blue",ylab="# haplotype",main="CEU-N=99",xlab="chr1-500bp-window")
+plot(d2[,1],type="n",ylab="# haplotype",,main="CHB+CEU",xlab="chr1-500bp-window")
+lines(d1[,1],type="l",col="red")
+lines(d2[,1],type="l",col="blue")
+dev.off()
+
+png("hapcount5000.png")
+par(mfrow=c(3,1))
+plot(d1[,1],type="l",col="red",ylab="# haplotype",main="CHB-N=103",xlab="chr1-10000bp-window")
+plot(d2[,1],type="l",col="blue",ylab="# haplotype",main="CEU-N=99",xlab="chr1-10000bp-window")
+plot(d3[,1],type="l",col="green",ylab="# haplotype",main="YRI-N=108",xlab="chr1-10000bp-window")
+dev.off()
+
+
+
+d1<-read.table("chr1.CHB.hap.txt")
+d2<-read.table("chr1.CEU.hap.txt")
+d3<-read.table("chr1.YRI.hap.txt")
+
+quantile(d1[,1])
+
+max(d1[,1])/206
+max(d2[,1])/198
+max(d3[,1])/216
+png("hapcount5000.png")
+par(mfrow=c(3,1))
+plot(d1[,1]/206,type="l",col="red",ylab="# haplotype",main="CHB-N=103",xlab="chr1-5000bp-window",ylim=c(0,1))
+plot(d2[,1]/198,type="l",col="blue",ylab="# haplotype",main="CEU-N=99",xlab="chr1-5000bp-window",ylim=c(0,1))
+plot(d3[,1]/216,type="l",col="blue",ylab="# haplotype",main="YRI-N=108",xlab="chr1-5000bp-window",ylim=c(0,1))
+dev.off()
+
+
+
+
+
+median(d1[d1>0])
+mean(d1[d1>0])
+plot(density(d1[d1>0]),xlim=c(0,10))
+
+quantile(d1[d1>0])
+mean
