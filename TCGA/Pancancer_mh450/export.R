4843c2938c8d47429d554ef31d2576ce8cc82de2
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-03-10 16:12:40 -0500

    Update export.R

--- TCGA/Pancancer_mh450/export.R
+++ TCGA/Pancancer_mh450/export.R
@@ -1,3 +1,7 @@
+#####################################################################
+#####################################################################
+#####################################################################
+
 setwd("/mnt/bigdata/Genetic/Projects/shg047/methylation/Pancancer")
 files=list.files(pattern="*gdc_hg38.txt$",recursive = T)
 methdata<-c()
@@ -9,3 +13,18 @@ for(i in 1:length(files)){
 colnames(methdata)<-files
 save(methdata,file="methdata.pancancer.RData")
 save.image("methdata.pancancer.env.RData")
+
+#####################################################################
+#####################################################################
+#####################################################################
+
+files=list.files(pattern="*.FPKM-UQ.txt$",recursive = T)
+rnaseqdata<-c()
+for(i in 1:length(files)){
+  temp<-read.table(files[i],head=F,sep="\t",row.names = 1)
+  rnaseqdata<-cbind(rnaseqdata,temp[,1])
+  print(i)
+}
+colnames(rnaseqdata)<-files
+save(rnaseqdata,file="rnaseqdata.pancancer.RData")
+save.image("rnaseqdata.pancancer.env.RData")
