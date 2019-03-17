7b2b7f7990d4c18ddab8036fe0a57f46e494d8f4
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-03-17 00:55:35 -0500

    Update VEPmerge.pl

--- LungBrainMetastasis/bin/VEPmerge.pl
+++ LungBrainMetastasis/bin/VEPmerge.pl
@@ -6,6 +6,7 @@ sub uniq {
 open F,"BLM-VEP.hg19.txt";
 my %data;
 while(<F>){
+next if $_ =~ /Location/i;
 my(undef,$var,undef,$anno)=split/\s+/;
 my @anno=split/\,/,$anno;
 push(@{$data{$var}},@anno);
@@ -13,6 +14,8 @@ push(@{$data{$var}},@anno);
 foreach my $var(sort keys %data){
         my @anno = uniq @{$data{$var}};
     my $anno=join(";",@anno);
-    my ($chr,$start,$end)=split/[:|-]/,$var;
+    my ($chr,$pos,undef)=split/[:|-]/,$var;
+    my $start=$pos-1;
+    my $end=$pos;
     print "chr$chr\t$start\t$end\t$anno\n";
 }
