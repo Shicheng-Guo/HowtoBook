ced9eaa727873b4d6948b8d5f547fbb9882a6751
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-04-06 20:59:56 -0500

    Create lusc.run.R

---
+++ TCGA/lusc/lusc.run.R
@@ -0,0 +1,131 @@
+setwd("/mnt/bigdata/Genetic/Projects/shg047/methylation/Pancancer")
+
+install.packages("pROC")
+install.packages("ggplot2")
+library("pROC")
+library("ggplot2")
+id2phen4<-function(filename){
+  library("stringr")
+  as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+}
+
+id2phen3<-function(filename){
+  library("stringr")
+  as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*"))
+}
+
+id2bin<-function(filename){
+  library("stringr")
+  filename<-as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+  as.numeric(lapply(strsplit(filename,"-"),function(x) x[4]))
+}
+
+id2pid<-function(filename){
+  library("stringr")
+  filename<-as.array(str_extract(filename,"edu_...."))
+  unlist(lapply(filename,function(x) unlist(strsplit(x,"[_]"))[2]))
+}
+
+RawNARemove<-function(data,missratio=0.3){
+  threshold<-(missratio)*ncol(data)
+  NaRaw<-which(apply(data,1,function(x) sum(is.na(x))>=threshold))
+  zero<-which(apply(data,1,function(x) all(x==0))==T)
+  NaRAW<-c(NaRaw,zero)
+  if(length(NaRAW)>0){
+    output<-data[-NaRAW,]
+  }else{
+    output<-data;
+  }
+  output
+}
+
+
+Table2Generator = function(methdata){
+  seq.case = which(methdata[,1] ==1)
+  seq.control = which(methdata[,1] == 11)
+  #Mean Case, Mean Control, Pvalue and Adjusted Pvalue
+  McaM = apply(methdata[,-1],2,function(x) {return( mean(x[seq.case], na.rm=T))} )
+  McoM = apply(methdata[,-1],2,function(x) {return( mean(x[seq.control], na.rm=T))} )
+  Pvalue=apply(methdata[,-1],2,function(x) {return( wilcox.test(x[seq.control], x[seq.case],na.rm=T)$p.value)})
+  Pvalue=p.adjust(Pvalue,method="fdr")
+  #Logistic regression analysis
+  OR =c()
+  CI.upper = c()
+  CI.lower = c()
+  Logistic.P = c()
+  Sens=c()
+  Spec=c()
+  AUC =c()
+  for(i in 1:(ncol(methdata)-1 )){
+    temp = methdata[,c(1,i+1 )]
+    temp[,1] = ifelse(temp[,1] ==1,1,0)
+    temp[,1] = as.factor(temp[,1])
+    glm.fit  = glm(temp[,1] ~ temp[,2], data = temp, family = "binomial")
+    OR[i] = log(exp(summary(glm.fit)$coefficients[2,1]),base = 10)
+    Logistic.P[i] = summary(glm.fit)$coefficients[2,4]
+    CI.upper[i]=log(exp(confint(glm.fit)[2,2]),base = 10)
+    CI.lower[i] = log(exp(confint(glm.fit)[2,1]),base = 10)
+    #Do the analysis of the sens, spec, and AUC
+    predicted.value = predict(glm.fit)
+    predicted.data  = data.frame(Type=na.omit(temp)[,1], predicted.value)
+    logistic.rocobj  = roc(predicted.data$Type, predicted.data$predicted.value,smooth = FALSE)
+    logistic.rocdata = data.frame(Sens = logistic.rocobj$sensitivities, Spec = logistic.rocobj$specificities)
+    AUC[i] = logistic.rocobj$auc[[1]]
+    #Find the best Sens and Spec
+    logistic.rocdata[,3] = logistic.rocdata[,1] + logistic.rocdata[,2]
+    seq.max = which(logistic.rocdata[,3] == max(logistic.rocdata[,3]))
+    Sens[i] = logistic.rocdata[seq.max,1]
+    Spec[i] = logistic.rocdata[seq.max,2]
+    print(i)
+  }
+  Logistic.P = p.adjust(Logistic.P, method = "fdr")
+  options(digits = 2)
+  Table = data.frame(McaM, McoM, Pvalue, OR, CI.upper, CI.lower, Logistic.P, Sens,Spec, AUC)
+  return(Table)
+}
+
+
+ColNARemove<-function(data,missratio=0.3){
+  threshold<-(missratio)*dim(data)[1]
+  NaCol<-which(apply(data,2,function(x) sum(is.na(x))>threshold))
+  zero<-which(apply(data,2,function(x) all(x==0))==T)
+  NaCOL<-c(NaCol,zero)
+  if(length(NaCOL)>0){
+    data1<-data[,-NaCOL]
+  }else{
+    data1<-data;
+  }
+  data1
+}
+
+setwd("/mnt/bigdata/Genetic/Projects/shg047/methylation/Pancancer")
+load("methdata.pancancer.RData")
+methdata[1:5,1:5]
+phen4<-id2phen4(colnames(methdata))
+phen3<-id2phen3(colnames(methdata))
+bin<-id2bin(colnames(methdata))
+pid<-id2pid(colnames(methdata))
+phen<-data.frame(phen4=phen4,phen3=phen3,pid=pid,bin=bin)
+exclude<-which(c(phen$bin !=1 & phen$bin !=11))
+phen<-phen[-exclude,]
+input<-methdata[,-exclude]
+Seq<-paste(phen$pid,phen$bin,sep="-")
+head(phen)
+input[1:5,1:5]
+
+LUSC<-grep("LUSC",colnames(input))
+newinput<-input[,LUSC]
+newphen<-phen[LUSC,]
+newinput[1:5,1:5]
+head(newphen)
+methdata=data.frame(newphen$bin,t(newinput))
+methdata[1:5,1:5]
+newmethdata=ColNARemove(methdata)
+newmethdata[1:5,1:5]
+rlt<-Table2Generator(newmethdata)
+map<-read.table("/mnt/bigdata/Genetic/Projects/shg047/db/hg19/GPL13534_450K_hg19.bed",sep="\t")
+newrlt<-data.frame(map[match(rownames(rlt),map[,4]),],rlt)
+head(newrlt)
+write.table(newrlt,file="TCGA_LUSC_meth450_marker.txt",col.names=NA,row.names = T,quote=F,sep="\t")
+
+
