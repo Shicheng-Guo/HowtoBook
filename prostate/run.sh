57ee13615fa3350db21db49abd41ef7a3af64559
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-04-24 01:18:34 -0500

    Update run.sh

--- prostate/run.sh
+++ prostate/run.sh
@@ -1,38 +1,3 @@
-for i in `ls *.strelka.somatic.vcf.gz`; do bcftools view -f PASS $i | wc -l ; done
-
-for i in `ls *.strelka.somatic.vcf.gz`
-do
-bcftools view -H -f PASS $i | awk '{print $1,$2,$3,$4,$5}' OFS="\t" >> prostate.vep.input
-echo $i
-done
-
-for i in `ls *.strelka.somatic.vcf.gz`
-do
-bcftools view -H -f PASS $i | awk '{print $1,$2-1,$2,$1":"$2-1"-"$2}' OFS="\t" > $i.anno.bed
-echo $i
-done
-
-perl vep_merge2.pl > prostate.somaticMutation.gene.txt
-perl -p -i -e 's/;-//g' prostate.somaticMutation.gene.txt
-
-mkdir temp
-for i in `ls *strelka.somatic.vcf.gz`
-do
-echo \#PBS -N $i  > $i.job
-echo \#PBS -l nodes=1:ppn=1 >> $i.job
-echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
-echo \#PBS -m abe  >> $i.job
-echo \#PBS -o $(pwd)/temp/ >>$i.job
-echo \#PBS -e $(pwd)/temp/ >>$i.job
-echo cd $(pwd) >> $i.job
-echo java -Xmx4g -jar ~/hpc/tools/snpEff/snpEff.jar "GRCh37.75" $i \> $i.anno.vcf >> $i.job 
-qsub $i.job
-done
-
-
-
-
-
 
 python configureStrelkaSomaticWorkflow.py --normalBam baihaixing_control.recal.bam --tumorBam baihaixing_T1.recal.bam --referenceFasta /thinker/storage/udata/bing/GATK/hg19/ucsc.hg19.fasta --exome --callRegions /thinker/storage/udata/bing/bed/xGenExomeResearchPaneltargets.sorted.ip100.merged.bed.gz --runDir baihaixing_T1_strelka_somatic
 
@@ -72,10 +37,9 @@ do
 bcftools view -f PASS $i -o $i.pass.vcf 
 bgzip $i.pass.vcf
 tabix -p vcf $i.pass.vcf.gz
-bcftools view -H -f PASS $i | awk '{print $1,$2-1,$2,$1":"$2-1"-",$3,$4,$5}' OFS="\t" > $i.bed
+bcftools view -H -f PASS $i.pass.vcf.gz | awk '{print $1,$2-1,$2,$1":"$2-1"-",$3,$4,$5}' OFS="\t" | sort -u > $i.bed
 echo $i
 done 
-
 
 ### pick up all the somatic mutations from vardict
 for i in `ls *.vardict.vcf.gz`
@@ -83,31 +47,48 @@ do
 bcftools view -i 'INFO/STATUS !="Germline"' $i -o $i.pass.vcf 
 bgzip $i.pass.vcf
 tabix -p vcf $i.pass.vcf.gz
-bcftools view -H -f PASS $i.pass.vcf.gz |awk '{print $1,$2-1,$2,$1":"$2-1"-"$2,$3,$4,$5}' OFS="\t" > $i.bed
+bcftools view -H -f PASS $i.pass.vcf.gz |awk '{print $1,$2-1,$2,$1":"$2-1"-"$2,$3,$4,$5}' OFS="\t" |sort -u > $i.bed
 echo $i
 done 
 
 ###### merge vardict and strelka
-ls *.vcf.gz.bed | awk -F"." '{print $1}' | sort -u > FileUnit.txt
-
-cat *vardict.strelka.overlap.bed | awk '{print $1,$3}' OFS="\t" | sort -u > valid.pos.txt
-
-for i in `ls *.strelka.somatic.vcf.gz.pass.vcf.gz`
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
 do
-bcftools view -T valid.pos.txt $i -Oz -o $i.vardit.overlap.vcf.gz
-tabix -p $i.vardit.overlap.vcf.gz
+bedtools intersect -wa -a $i.vardict.vcf.gz.bed -b $i.strelka.somatic.vcf.gz.bed | sort -u > $i.vardict.strelka.overlap.bed
+done
+
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
+do
 echo $i
 done
 
-for i in `ls *.strelka.somatic.vcf.gz.pass.vcf.gz`
+###### merge vardict and strelka
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
 do
+bedtools intersect -wa -a $i.vardict.vcf.gz.bed -b $i.strelka.somatic.vcf.gz.bed | sort -u | awk '{print $1,$3}' OFS="\t" > $i.vardict.strelka.overlap.pos
 echo $i
-bcftools view -T valid.pos.txt $i -Oz -o $i.vardit.overlap.vcf.gz
-tabix -p vcf $i.vardit.overlap.vcf.gz
+done
+
+
+ls *.vcf.gz.bed | awk -F"." '{print $1}' | sort -u > FileUnit.txt
+cat *vardict.strelka.overlap.bed | awk '{print $1,$3}' OFS="\t" | sort -u > valid.pos.txt
+
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
+do
+bcftools view -T $i.vardict.strelka.overlap.pos $i.strelka.somatic.vcf.gz.pass.vcf.gz -Oz -o $i.strelka.vardit.overlap.vcf.gz
+tabix -p vcf $i.strelka.vardit.overlap.vcf.gz
+echo $i
+done
+
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
+do
+bcftools view -T $i.vardict.strelka.overlap.pos $i.vardict.vcf.gz.pass.vcf.gz -Oz -o $i.vardict.strelka.overlap.vcf.gz
+tabix -p vcf $i.vardict.strelka.overlap.vcf.gz
+echo $i
 done
 
 mkdir temp
-for i in `ls *strelka.somatic.vcf.gz.pass.vcf.gz.vardit.overlap.vcf.gz`
+for i in `ls *strelka.vardit.overlap.vcf.gz`
 do
 echo \#PBS -N $i  > $i.job
 echo \#PBS -l nodes=1:ppn=8 >> $i.job
@@ -120,25 +101,40 @@ echo java -Xmx16g -jar ~/hpc/tools/snpEff/snpEff.jar "GRCh37.75" $i \> $i.anno.v
 qsub $i.job
 done
 
-for i in `ls *.strelka.somatic.vcf.gz.pass.vcf.gz.vardit.overlap.vcf.gz.anno.vcf`
+mkdir temp
+for i in `ls *.vardict.strelka.overlap.vcf.gz`
+do
+echo \#PBS -N $i  > $i.job
+echo \#PBS -l nodes=1:ppn=8 >> $i.job
+echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
+echo \#PBS -m abe  >> $i.job
+echo \#PBS -o $(pwd)/temp/ >>$i.job
+echo \#PBS -e $(pwd)/temp/ >>$i.job
+echo cd $(pwd) >> $i.job
+echo java -Xmx16g -jar ~/hpc/tools/snpEff/snpEff.jar "GRCh37.75" $i \> $i.anno.vcf >> $i.job 
+qsub $i.job
+done
+
+#################################################################################################################################################
+#################################################################################################################################################
+for i in `ls *.strelka.vardit.overlap.vcf.gz.anno.vcf`
 do
 java -jar ~/hpc/tools/snpEff/SnpSift.jar extractFields $i CHROM POS REF ALT "ANN[*].GENE:" | awk -F'[\t|]' '{print $1,$2,$3,$4,$8}' OFS="\t" > $i.hg19.anno.bed
 java -jar ~/hpc/tools/snpEff/SnpSift.jar tstv $i > $i.hg19.tstv.bed
 done
 
-for i in `ls *.strelka.somatic.vcf.gz.pass.vcf.gz.vardit.overlap.vcf.gz.anno.vcf`
+for i in `ls *vardict.strelka.overlap.vcf.gz.anno.vcf`
 do
-grep -v '#' $i|  perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1" if $_=~/SGT=(\w+->\w+);/}' > $i.hg19.SGT.bed
-done
-
- 
-for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14ming_T1 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
-do
-bcftools view -T $i.vardict.strelka.overlap.bed $i.vardict.vcf.gz.pass.vcf.gz -Oz -o $i.vardict.strelka.overlap.vcf.gz
-tabix -p vcf $i.vardict.strelka.overlap.vcf.gz
-zcat $i.vardict.strelka.overlap.vcf.gz | grep -v '#' | perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1" if $_=~/TYPE=(\w+);/}' > $i.vardit.strelka.overlap.TYPE.hg19.bed
+grep -v '#' $i | perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1\t$2\t$3\t$4\t$5" if $_=~/TYPE=(\w+);.*\|(\w+)\|(\w+)\|(\w+)\|(ENSG\w+)/}' > $i.hg19.bed
 echo $i
 done
+
+for i in P10_T1 P11_T1 P12_T1 P12_T2 P12_T3 P13_T1 P13_T2 P13_T4 P14_T1 P14_T3 P14_T4 P15_T1 P16_T1 P16_T2 P1_T1 P1_T2 P2_T2 P2_T3 P2_T4 P3_T2 P3_T3 P3_T4 P4_T1 P4_T3 P5_T1 P5_T2 P5_T3 P6_T1 P6_T2 P6_T4 P7_T1 P8_T1 P8_T2 P9_T2 P9_T3 P9_T4 
+do
+grep -v '#' $i.vardict.strelka.overlap.vcf.gz.anno.vcf| perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1" if $_=~/TYPE=(\w+);/}' > $i.vardit.strelka.overlap.type.hg19.bed
+echo $i
+done
+
 
 mkdir temp
 for i in `ls *vardict.strelka.overlap.vcf.gz`
@@ -160,5 +156,4 @@ grep -v '#' $i | perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1\t$2\t$3\t$4\
 echo $i
 done
 
-
-
+grep -v '#' P3_T2.vardict.strelka.overlap.vcf.gz.anno.vcf | perl -lane '{print "@F[0]\t@F[1]\t@F[3]\t@F[4]\t$1\t$2" if $_=~/TYPE=(\w+);.*(ENSG\w+)/}'
