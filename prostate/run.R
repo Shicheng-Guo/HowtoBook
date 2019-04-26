07dfeabbdf5ffe2ae5fee1b407a114ac80cb25ef
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-04-25 19:00:26 -0500

    Update run.R

--- prostate/run.R
+++ prostate/run.R
@@ -139,7 +139,6 @@ pratchet <- pratchet(input)
 plot(root(pratchet,outgroup="Germline"),cex=2)
 }
 }
-
 data<-read.table("symbol.snv.txt",head=T,sep="\t",row.names = 1)
 data<-t(data.matrix(data))
 newdata<-data
@@ -149,8 +148,11 @@ newdata<-rbind(newdata,Germline=0)
 input<-as.phyDat(newdata, type="USER", levels = c(0, 1))
 pratchet <- pratchet(input)
 plot(root(pratchet,outgroup="Germline"),cex=0.85)
-
 roundPhylogram(root(pratchet,outgroup="Germline"),cex=0.25)
+
+
+
+
 # roundPhylogram(pratchet)
 # plot(pratchet)
 # plot(unroot(pratchet),type="unrooted",no.margin=TRUE,lab4ut="axial",edge.width=2,cex=1.2)
@@ -210,24 +212,96 @@ for(id in iid){
   }
 }
 
-for(id in iid){
+
+################################################################################
+library(gplots)
+library(VennDiagram)
+count<-read.table("prostat.type.txt",head=T,row.names = 1)
+iid<-unlist(lapply(strsplit(as.character(colnames(count)),"[.]"),function(x) x[1]))
+data<-data.matrix(count)
+head(data)
+data[data==1]<-0
+data[data>0]<-1
+colnames(data)<-label
+head(data)
+sid<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[1]))
+sid
+
+for(id in unique(sid)){
   inr<-which(sid %in% id)
-  if(length(inr)==4){
+  if(length(inr)==3){
     input<-data[,inr]
-    input[input ==1]=0
-    input[input>1] = 1
     head(input)
+    rmv<-which(rowSums(input)==0)
+    length(rmv)/nrow(input)
     input<-input[-which(rowSums(input)==0),]
     colnames(input)
     T1<-rownames(input)[which(input[,1]>0)]
     T2<-rownames(input)[which(input[,2]>0)]
     T3<-rownames(input)[which(input[,3]>0)]
-    T4<-rownames(input)[which(input[,4]>0)]
-    vd <- venn.diagram(list(T1=T1, T2=T2),fill = 2:5,filename=NULL)
-    pdf(paste("Figure",id,"venn.pdf",sep="."))
+    vd <- venn.diagram(list(T1=T1, T2=T2, T3=T3),fill = 2:4,filename=NULL)
+    pdf(paste("Figure",id,"point.venn.pdf",sep="."))
     grid.draw(vd)
     dev.off()
   }
+}
+
+
+for(id in unique(sid)){
+  inr<-which(sid %in% id)
+  if(length(inr)==2){
+    input<-data[,inr]
+    head(input)
+    rmv<-which(rowSums(input)==0)
+    length(rmv)/nrow(input)
+    input<-input[-which(rowSums(input)==0),]
+    colnames(input)
+    T1<-rownames(input)[which(input[,1]>0)]
+    T2<-rownames(input)[which(input[,2]>0)]
+    vd <- venn.diagram(list(T1=T1, T2=T2),fill = 2:3,filename=NULL)
+    pdf(paste("Figure",id,"point.venn.pdf",sep="."))
+    grid.draw(vd)
+    dev.off()
+  }
+}
+
+
+count<-read.table("prostat.type.txt",head=T,row.names = 1)
+iid<-unlist(lapply(strsplit(as.character(colnames(count)),"[.]"),function(x) x[1]))
+sid<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[1]))
+data<-data.matrix(count)
+head(data)
+data[data==1]<-0
+data[data>0]<-1
+colnames(data)<-iid
+data<-t(data)
+newdata<-rbind(data,Germline=0)
+newdata[,1]
+input<-as.phyDat(newdata, type="USER", levels = c(0, 1))
+pratchet <- pratchet(input)
+pdf("Figure_Prostate_Root_Phylogenetic_treemode.SNV.pdf")
+plot(root(pratchet,outgroup="Germline"),cex=0.85)
+roundPhylogram(root(pratchet,outgroup="Germline"),cex=0.25)
+dev.off()
+
+iid<-unlist(lapply(strsplit(rownames(data),"_"),function(x) x[1]))
+for(i in unique(iid)){
+  iiid<-which(iid %in% i)
+  if(length(iiid)>2){
+    newdata<-data[iiid,]
+    newdata[1:2,1:2]
+    newdata<-rbind(newdata,Germline=0)
+    input<-as.phyDat(newdata, type="USER", levels = c(0, 1))
+    pratchet <- pratchet(input)
+    treeRatchetBL<- acctran(pratchet,input)
+    write.tree(treeRatchetBL,file=paste(i,"pratchet.tree",sep="."))
+    pdf(paste(i,"pratchet.raw.tree.pdf",sep="."))
+    plot(root(treeRatchetBL,outgroup="Germline"),cex=0.95)
+    dev.off()
+    pdf(paste(i,"pratchet.round.tree.pdf",sep="."))
+    roundPhylogram(root(treeRatchetBL,outgroup="Germline"),cex=0.25)
+    dev.off()
+    }
 }
 
 #################################################################################################################
@@ -252,7 +326,6 @@ for(id in unique(iid)){
   Rowname<-c(Rowname,id)
   }
 }
-
 rownames(tps)<-Rowname
 colnames(tps)<-c("trunk","share","private")
 TPS<-tps/(rowSums(tps))
@@ -260,43 +333,93 @@ write.table(tps,file="trunk.share.private.tps.num.txt",sep="\t",col.names = NA,r
 write.table(TPS,file="trunk.share.private.tps.prop.txt",sep="\t",col.names = NA,row.names = T,quote=F)
 
 #################################################################################################################
+###########################     SNV based Sum(sub-Sample)       #########################################
 #################################################################################################################
+sheet7= read_excel("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/prostate/Result.xlsx",sheet = 7)
+sheet7= as.data.frame(sheet7)
+data<-xl2matrix(sheet7)
+head(data)
+data[data=="_"]<-0
+data[data!=0]<-1
+head(data)
+iid<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[1]))
+iid
+head(data)
+output1<-c()
+output2<-c()
+for(id in unique(iid)){
+    print(id)
+    inr<-which(iid %in% id)
+    input<-data[,inr]
+    head(data.frame(input))
+    idata1<-unlist(apply(data.frame(input),1,function(x) sum(as.numeric(x))>0))
+    idata2<-unlist(apply(data.frame(input),1,function(x) sum(as.numeric(x))))
+    output1<-cbind(output1,idata1)
+    output2<-cbind(output2,idata2)
+    
+}
+colnames(output1)<-unique(iid)
+colnames(output2)<-unique(iid)
+output1[output1=="TRUE"]<-1
+output1[output1=="FALSE"]<-0
+head(output1)
+head(output2)
+write.table(output1,file="prostate.mutationProfile.SNV.01.txt",sep="\t",col.names = NA,row.names = T,quote=F)
+write.table(output2,file="prostate.mutationProfile.SNV.num.txt",sep="\t",col.names = NA,row.names = T,quote=F)
 
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
 setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/prostate/vcf")
 data<-read.table("symbol.snv.txt",head=T,sep="\t",row.names = 1)
 data<-data.matrix(data)
 sid<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[1]))
 sid
-output<-c()
+output1<-c()
+output2<-c()
 for(id in unique(iid)){
-    print(id)
-    inr<-which(sid %in% id)
-    input<-data[,inr]
-    input[input ==1]=0
-    input[input>1] = 1
-    head(input)
-    idata<-unlist(apply(data.frame(input),1,function(x) sum(x)>0))
-    output<-cbind(output,idata)
+  print(id)
+  inr<-which(sid %in% id)
+  input<-data[,inr]
+  input[input ==1]=0
+  input[input>1] = 1
+  head(input)
+  idata1<-unlist(apply(data.frame(input),1,function(x) sum(x)>0))
+  idata2<-unlist(apply(data.frame(input),1,function(x) sum(x)))
+  output1<-cbind(output1,idata1)
+  output2<-cbind(output2,idata2)
+  
 }
-colnames(output)<-unique(iid)
-output[output=="TRUE"]<-1
-output[output=="FALSE"]<-0
-head(output)
-write.table(output,file="prostate.mutationProfile.txt",sep="\t",col.names = NA,row.names = T,quote=F)
-
-
-
+colnames(output1)<-unique(iid)
+colnames(output2)<-unique(iid)
+output1[output1=="TRUE"]<-1
+output1[output1=="FALSE"]<-0
+head(output1)
+head(output2)
+write.table(output1,file="prostate.mutationProfile.01.txt",sep="\t",col.names = NA,row.names = T,quote=F)
+write.table(output2,file="prostate.mutationProfile.num.txt",sep="\t",col.names = NA,row.names = T,quote=F)
 #################################################################################################################
 #################################################################################################################
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
 BiocManager::install("clusterProfiler", version = "3.8")
+BiocManager::install("ReactomePA", version = "3.8")
+BiocManager::install("urltools", version = "3.8")
+BiocManager::install("backports", version = "3.8")
+BiocManager::install("clusterProfiler", version = "3.8")
+BiocManager::install("igraph", version = "3.8")
+BiocManager::install("fgsea", version = "3.8")
+BiocManager::install("DOSE", version = "3.8")
+BiocManager::install("ggraph", version = "3.8")
+BiocManager::install("enrichplot", version = "3.8")
+BiocManager::install("doing", version = "3.8")
+BiocManager::install("data.table", version = "3.8")
+library("devtools")
+library("backports")
 library("fgsea")
 library("ReactomePA")
 library("clusterProfiler")
-library(org.Hs.eg.db)
-library(DOSE)
-library(ReactomePA)
+library("org.Hs.eg.db")
+library("DOSE")
+library("ReactomePA")
 
 data(geneList)
 de <- names(geneList)[abs(geneList) > 1.5]
@@ -307,7 +430,6 @@ dotplot(x, showCategory=15)
 emapplot(x)
 cnetplot(x, categorySize="pvalue", foldChange=geneList)
 
-
 y <- gsePathway(geneList, nPerm=10000,pvalueCutoff=0.2,pAdjustMethod="BH", verbose=FALSE)
 res <- as.data.frame(y)
 head(res)
@@ -316,10 +438,182 @@ gseaplot(y, geneSetID = "R-HSA-69242")
 viewPathway("E2F mediated regulation of DNA replication", readable=TRUE, foldChange=geneList)
 
 
-
 #################################################################################################################
 #################################################################################################################
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("CancerMutationAnalysis", version = "3.8")
+library("CancerMutationAnalysis")
+data(WoodBreast07)
+data(WoodColon07)
+data(JonesPancreas08)
+data(ParsonsGBM08)
+data(ParsonsMB11)
+
+head(GeneAlterGBM)
+head(GeneCovGBM)
+head(GeneCovGBM)
+ScoresGBM <- cma.scores(cma.alter = GeneAlterGBM,
+                        cma.cov = GeneCovGBM,
+                        cma.samp = GeneSampGBM,
+                        passenger.rates = BackRatesGBM["MedianRates",])
+head(ScoresGBM)
+set.seed(188310)
+FdrGBM <-  cma.fdr(cma.alter = GeneAlterGBM,
+                   cma.cov = GeneCovGBM,
+                   cma.samp = GeneSampGBM,
+                   scores = "logLRT",
+                   passenger.rates = BackRatesGBM["MedianRates",],
+                   showFigure=TRUE,
+                   cutoffFdr=0.1,
+                   M = 5)
+
+head(FdrGBM[["logLRT"]])
+
+#################################################################################################################
+##################### Mapp to COSMIC database Profile ############################################################
+#################################################################################################################
+devtools::install_github("kgori/sigfit", args = "--preclean", build_vignettes = TRUE)
+
+
+
+
+
+#################################################################################################################
+#################################################################################################################
+library("readxl")
+xl2matrix<-function(x){
+  rownames(x) <-x[,1]
+  x<-x[,2:ncol(x)]
+  x
+}
+
+sheet1= read_excel("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/prostate/Result.xlsx",sheet = 1)
+sheet7= read_excel("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/prostate/Result.xlsx",sheet = 7)
+sheet9= read_excel("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/prostate/Result.xlsx",sheet = 9)
+sheet1= as.data.frame(sheet1)
+sheet7= as.data.frame(sheet7)
+sheet9= as.data.frame(sheet9)
+sheet1<-xl2matrix(sheet1)
+sheet7<-xl2matrix(sheet7)
+sheet9<-xl2matrix(sheet9)
+head(sheet1)
+head(sheet7)
+head(sheet9)
+sheet7[sheet7=="_"]<-0
+sheet7[sheet7!="_"]<-1
+############################################
+## Mutation with age
+Rlt<-c()
+for(i in 1:nrow(sheet9)){
+  rlt<-summary(glm(as.numeric(sheet9[i,])~sheet1$Age))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet9)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet9)~sheet1$Age))
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet9)){
+  rlt<-summary(glm(as.numeric(sheet9[i,])~sheet1$fPSA_tPSA))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet9)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet9)~sheet1$fPSA_tPSA))
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet9)){
+  rlt<-summary(glm(as.numeric(sheet9[i,])~sheet1$Gleason_Score_Value))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet9)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)
+sum(Rlt[,1]<0)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet9)~sheet1$Gleason_Score_Value))
+plot(colSums(sheet9)~sheet1$Gleason_Score_Value)
+t.test(colSums(sheet9)[which(sheet1$Gleason_Score_Value>7.5)],colSums(sheet9)[which(sheet1$Gleason_Score_Value<7.5)])
+
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet9)){
+  rlt<-summary(glm(as.numeric(sheet9[i,])~sheet1$Metastasis_binary))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet9)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)
+sum(Rlt[,1]<0)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(glm(colSums(sheet9)~sheet1$Metastasis_binary))
+plot(colSums(sheet9)~sheet1$Gleason_Score_Value)
+t.test(colSums(sheet9)[which(sheet1$Gleason_Score_Value>7.5)],colSums(sheet9)[which(sheet1$Gleason_Score_Value<7.5)])
+
+
+###############################
+Rlt<-c()
+head(sheet7)
+for(i in 1:nrow(sheet7)){
+  rlt<-summary(glm(as.numeric(sheet7[i,])~sheet1$Age))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet7)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet7)~sheet1$Age))
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet7)){
+  rlt<-summary(glm(as.numeric(sheet7[i,])~sheet1$fPSA_tPSA))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet7)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet7)~sheet1$fPSA_tPSA))
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet7)){
+  rlt<-summary(glm(as.numeric(sheet7[i,])~sheet1$Gleason_Score_Value))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet7)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)
+sum(Rlt[,1]<0)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(lm(colSums(sheet7)~sheet1$Gleason_Score_Value))
+plot(colSums(sheet7)~sheet1$Gleason_Score_Value)
+t.test(colSums(sheet7)[which(sheet1$Gleason_Score_Value>7.5)],colSums(sheet7)[which(sheet1$Gleason_Score_Value<7.5)])
+
+
+## Mutation with fPSA/tPSA
+Rlt<-c()
+for(i in 1:nrow(sheet7)){
+  rlt<-summary(glm(as.numeric(sheet7[i,])~sheet1$Metastasis_binary))$coefficients[2,]
+  Rlt<-rbind(Rlt,rlt)
+}
+rownames(Rlt)<-rownames(sheet7)
+hist(log(Rlt[,4],10))
+head(Rlt[order(Rlt[,4]),])
+sum(Rlt[,1]>0)
+sum(Rlt[,1]<0)
+sum(Rlt[,1]>0)/sum(Rlt[,1]<0)
+summary(glm(colSums(sheet7)~sheet1$Metastasis_binary))
+plot(colSums(sheet7)~sheet1$Gleason_Score_Value)
+t.test(colSums(sheet7)[which(sheet1$Gleason_Score_Value>7.5)],colSums(sheet7)[which(sheet1$Gleason_Score_Value<7.5)])
+
+
 
