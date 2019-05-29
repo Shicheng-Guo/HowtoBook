55538a6389c78f30a7e59ac3cd0772129b73e03d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-28 21:20:08 -0500

    Update drug_clinical.R

--- TCGA/drug_response/drug_clinical.R
+++ TCGA/drug_response/drug_clinical.R
@@ -1,4 +1,9 @@
-query <- GDCquery(data.category = "Clinical",file.type = "xml")
+library("TCGAbiolinks")
+pid<-TCGAbiolinks:::getGDCprojects()$project_id
+pid<-pid[grep("TCGA",pid)]
+for(i in pid){
+query <- GDCquery(project=i,data.category = "Clinical",file.type = "xml")
 GDCdownload(query)
 clinical <- GDCprepare_clinic(query,"patient")
 clinical.drug <- GDCprepare_clinic(query,"drug")
+}
