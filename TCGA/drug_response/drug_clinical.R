b210866c52769e97a354a7e2f58f4f80b0afc9ab
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-28 22:32:50 -0500

    Update drug_clinical.R

--- TCGA/drug_response/drug_clinical.R
+++ TCGA/drug_response/drug_clinical.R
@@ -1,17 +1,6 @@
 library("TCGAbiolinks")
 pid<-TCGAbiolinks:::getGDCprojects()$project_id
 pid<-pid[grep("TCGA",pid)]
-drugResponse<-c()
-for(i in pid){
-query <- GDCquery(project=i,data.category = "Clinical",file.type = "xml")
-GDCdownload(query)
-clinical.drug <- GDCprepare_clinic(query,"drug")
-temp<-drug2csv(clinical.drug)
-drugResponse<-rbind(drugResponse,temp)
-}
-
-
-TCGA-SARC
 
 drug2csv<-function(clinical.drug){
   bcr_patient_barcode<-clinical.drug$bcr_patient_barcode
@@ -24,3 +13,14 @@ drug2csv<-function(clinical.drug){
   new.clinical.drug<-data.frame(bcr_patient_barcode,therapy_types,drug_name,measure_of_response,days_to_drug_therapy_start,days_to_drug_therapy_end,therapy_ongoing)
   return(new.clinical.drug)
 }
+
+for(i in pid){
+query <- GDCquery(project=i,data.category = "Clinical",file.type = "xml")
+GDCdownload(query)
+clinical.drug <- GDCprepare_clinic(query,"drug")
+drugResponse<-drug2csv(clinical.drug)
+write.table(drugResponse,file=paste(i,".drugResponse.txt",sep=""),col.names=T,row.names=F,quote=F,sep="\t")
+save(drugResponse,file=paste(i,".drugResponse.RData",sep=""))
+}
+
+
