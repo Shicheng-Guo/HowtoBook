5388fa8bc2e44b27ad104eb251351e01bcae095f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-29 00:35:38 -0500

    Update responsePrediction.R

--- TCGA/drug_response/responsePrediction.R
+++ TCGA/drug_response/responsePrediction.R
@@ -1,5 +1,11 @@
-
+setwd("/home/guosa/hpc/project/TCGA")
 
 phen<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/drug_response/pancancer.chemotherapy.response.txt",head=T,sep="\t")
-head(phen)
+barcode<-read.table("/home/guosa/hpc/project/TCGA/pancancer/FPKM/barcode.txt",head=T,sep="\t")
 load("rnaseqdata.pancancer.env.RData")
+
+ncn<-barcode[match(unlist(lapply(colnames(rnaseqdata),function(x) unlist(strsplit(x,"[/]"))[2])),barcode$file_name<-gsub(".gz","",barcode$file_name)),]
+ncol<-match("cases.0.samples.0.submitter_id",colnames(ncn))
+colnames(rnaseq)<-ncn[,ncol]
+phen$ID<-paste(phen$bcr_patient_barcode,"-01",sep="")
+
