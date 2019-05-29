446c34aa9aad95719cc272ee464cc232f5870ca8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-29 00:11:02 -0500

    Update responsePrediction.R

--- TCGA/drug_response/responsePrediction.R
+++ TCGA/drug_response/responsePrediction.R
@@ -1,1 +1,5 @@
 
+
+phen<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/drug_response/pancancer.chemotherapy.response.txt",head=T,sep="\t")
+head(phen)
+load("rnaseqdata.pancancer.env.RData")
