3724080555ad46c6b76f80fa825701adf3d91729
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-29 00:39:42 -0500

    Create id2bin.R

---
+++ TCGA/bin/id2bin.R
@@ -0,0 +1,6 @@
+id2bin<-function(filename){
+  library("stringr")
+  filename<-as.array(str_extract(filename,"TCGA-[0-9|a-z|A-Z]*-[0-9|a-z|A-Z]*-[0-9]*"))
+  as.numeric(lapply(strsplit(filename,"-"),function(x) x[4]))
+}
+
