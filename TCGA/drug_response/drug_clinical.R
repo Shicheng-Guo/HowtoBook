3e8040ee4acb1ada2981376c4b99fbb43165a0ed
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-28 22:08:12 -0500

    Update drug_clinical.R

--- TCGA/drug_response/drug_clinical.R
+++ TCGA/drug_response/drug_clinical.R
@@ -1,10 +1,26 @@
 library("TCGAbiolinks")
 pid<-TCGAbiolinks:::getGDCprojects()$project_id
 pid<-pid[grep("TCGA",pid)]
+drugResponse<-c()
 for(i in pid){
 query <- GDCquery(project=i,data.category = "Clinical",file.type = "xml")
 GDCdownload(query)
 clinical.drug <- GDCprepare_clinic(query,"drug")
+temp<-drug2csv(clinical.drug)
+drugResponse<-rbind(drugResponse,temp)
 }
 
-# clinical <- GDCprepare_clinic(query,"patient")
+
+TCGA-SARC
+
+drug2csv<-function(clinical.drug){
+  bcr_patient_barcode<-clinical.drug$bcr_patient_barcode
+  therapy_types<-clinical.drug$therapy_types
+  drug_name<-clinical.drug$drug_name
+  measure_of_response<-clinical.drug$measure_of_response
+  days_to_drug_therapy_start<-clinical.drug$days_to_drug_therapy_start
+  days_to_drug_therapy_end<-clinical.drug$days_to_drug_therapy_end
+  therapy_ongoing<-clinical.drug$therapy_ongoing
+  new.clinical.drug<-data.frame(bcr_patient_barcode,therapy_types,drug_name,measure_of_response,days_to_drug_therapy_start,days_to_drug_therapy_end,therapy_ongoing)
+  return(new.clinical.drug)
+}
