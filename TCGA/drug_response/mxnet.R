0ba134327dd8ba8a26b551192b25a6575325f8f4
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-31 01:49:30 -0500

    Create mxnet.R

---
+++ TCGA/drug_response/mxnet.R
@@ -0,0 +1,16 @@
+install.packages('mlbench')
+require(mxnet)
+require(mlbench)
+data(Sonar, package="mlbench")
+Sonar[,61] = as.numeric(Sonar[,61])-1
+train.ind = c(1:50, 100:150)
+train.x = data.matrix(Sonar[train.ind, 1:60])
+train.y = Sonar[train.ind, 61]
+test.x = data.matrix(Sonar[-train.ind, 1:60])
+test.y = Sonar[-train.ind, 61]
+mx.set.seed(0)
+model <- mx.mlp(train.x, train.y, hidden_node=10, out_node=2, out_activation="softmax",num.round=20, array.batch.size=15, learning.rate=0.07, momentum=0.9,eval.metric=mx.metric.accuracy)
+graph.viz(model$symbol)
+preds = predict(model, test.x)
+pred.label = max.col(t(preds))-1
+table(pred.label, test.y)
