f5f1b59487e3223959daff042f5ab48b8a562fa9
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-01 21:30:31 -0500

    Update tsne.R

--- TCGA/Pancancer_miRNA/tsne.R
+++ TCGA/Pancancer_miRNA/tsne.R
@@ -1,3 +1,4 @@
+## Senarior-A
 load("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/TCGA/pancancer/miRNA/data/TCGA-Pancancer.miRNAseq.RData")
 load("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/TCGA/pancancer/miRNA/data/TCGA-Pancancer.miRNAseq.RData")
 library("tsne")
@@ -13,3 +14,16 @@ tsne_iris = tsne(t(miRNA), epoch_callback = ecb, perplexity=30)
 tsne_iris = tsne(t(miRNA), epoch_callback = ecb, perplexity=50)
 tsne_iris = tsne(t(miRNA), epoch_callback = ecb, perplexity=70)
 dev.off()
+## Scenario-B
+clic<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/clinical.txt",head=T,sep="\t")
+pid<-clic[match(id2phen3(colnames(miRNA)),clic[,1]),2]
+pid<-pid[grep("TCGA",pid)]
+input<-miRNA[,grep("TCGA",pid)]
+colors = rainbow(length(unique(pid)))
+names(colors) = unique(phen)
+ecb = function(x,y){plot(x,t='n'); text(x,labels=phen, col=colors[phen]) }
+pdf("pancancer.miRNA.tnse.pid.pdf")
+tsne_iris10 = tsne(t(miRNA), epoch_callback = ecb, perplexity=10,max_iter = 100)
+dev.off()
+
+
