82204d59a41b025f0228f6632e1cf3cf9afefe6d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-04 18:25:53 -0500

    Update run.sh

--- autism/2LOF/run.sh
+++ autism/2LOF/run.sh
@@ -2,4 +2,8 @@ cd /home/guosa/hpc/autism/data/2LOF
 mkdir 2LOF
 cp All* 2LOF
 plink --bfile All_samples_Exome_QC --recode vcf --out All_samples_Exome_QC
- 
+avinput="All_samples_Exome_QC.vcf"
+table_annovar.pl $avinput ~/hpc/tools/annovar/humandb/ --thread 12 -buildver hg19 --csvout -out $avinput -remove -protocol refGene,dbnsfp35c,gnomad_genome,gwasCatalog,gtexEqtlCluster -operation g,f,f,r,r -nastring . 
+
+
+
