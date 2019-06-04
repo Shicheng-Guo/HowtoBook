56771a1e1e67f1f0e3a2212fb1ef2a42cd286f40
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-04 17:38:23 -0500

    Update run.sh

--- autism/2LOF/run.sh
+++ autism/2LOF/run.sh
@@ -1,5 +1,5 @@
 cd /home/guosa/hpc/autism/data/2LOF
 mkdir 2LOF
 cp All* 2LOF
- plink --bfile All_samples_Exome_QC --recode vcf --out All_samples_Exome_QC
+plink --bfile All_samples_Exome_QC --recode vcf --out All_samples_Exome_QC
  
