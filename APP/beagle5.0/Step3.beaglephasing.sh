adb553d4048d3a10d048dfff9b23c4aaf3f2c2de
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-13 23:41:31 -0500

    Create Step3.beaglephasing.sh

---
+++ APP/beagle5.0/Step3.beaglephasing.sh
@@ -0,0 +1,13 @@
+mkdir temp
+for i in {1..22} X Y
+do
+echo \#PBS -N $i  > $i.job
+echo \#PBS -l nodes=1:ppn=16 >> $i.job
+echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
+echo \#PBS -m abe  >> $i.job
+echo \#PBS -o $(pwd)/temp/ >>$i.job
+echo \#PBS -e $(pwd)/temp/ >>$i.job
+echo cd $(pwd) >> $i.job
+echo java -Djava.io.tmpdir=./temp/ -Xmx32g -jar beagle.16May19.351.jar gt=All_samples_Exome_QC.temp.vcf.recode.clean.chr$i.vcf.recode.vcf ref=~/hpc/db/hg19/beagle/EUR/chr$i.1kg.phase3.v5a.EUR.vcf.gz map=~/hpc/db/hg19/beagle/plink.chr$i.GRCh37.map out=All_samples_Exome_QC.chr$i.vcf >>$i.job
+qsub $i.job
+done
