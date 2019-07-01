37db0339bfed21916d4c8f75525afce1a9770977
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-30 23:58:53 -0500

    Create PANC754.R

---
+++ PANC/PANC754.R
@@ -0,0 +1,30 @@
+load("rnaseqdata.pancancer.RData")
+input<-log(input+1,2)
+input<-RawNARemove(input)
+input<-RawZeroRemove(input)
+Seq<-paste(phen$project_id,phen$bin,sep="-")
+rlt<-c()
+coll<-c()
+for(i in 1:nrow(input)){
+  print(i)
+  mean<-tapply(as.numeric(input[i,]),Seq,function(x) mean(x,na.rm=T))
+  sd<-tapply(as.numeric(input[i,]),Seq,function(x) sd(x,na.rm=T))
+  num<-tapply(as.numeric(input[i,]),Seq,function(x) length(x))
+  m1i=mean[seq(1,length(mean),by=2)]
+  m2i=mean[seq(2,length(mean),by=2)]
+  sd1i=sd[seq(1,length(mean),by=2)]
+  sd2i=sd[seq(2,length(mean),by=2)]
+  n1i=num[seq(1,length(mean),by=2)]
+  n2i=num[seq(2,length(mean),by=2)]
+  Source<-unlist(lapply(strsplit(names(m1i),"_"),function(x) x[1]))
+  output<-data.frame(cbind(n1i,m1i,sd1i,n2i,m2i,sd2i))
+  output$source=Source
+  output<-na.omit(output)
+  es<-escalc(m1i=m1i, sd1i=sd1i, n1i=n1i, m2i=m2i, sd2i=sd2i, n2i=n2i,measure="MD",data=output)
+  md <- rma(es,slab=source,method = "REML", measure = "SMD",data=output)
+  rlt<-rbind(rlt,c(i,md$beta,md$pval,md$ci.lb,md$ci.ub,md$I2,md$tau2))
+  coll<-c(coll,i)
+}
+rownames(rlt)<-rownames(input)[coll]
+colnames(rlt)<-c("idx","beta","pval","cilb","ciub","i2","tau2")
+rlt<-data.frame(rlt)
