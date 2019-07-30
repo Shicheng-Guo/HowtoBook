bc1e38be6a365f76522ffbdb49a8f991a1a9462d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-30 11:34:13 -0500

    Update HR_OS.R

--- TCGA/Pancancer_FPKM_UQ/Survival/HR_OS.R
+++ TCGA/Pancancer_FPKM_UQ/Survival/HR_OS.R
@@ -1,3 +1,33 @@
+source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/GscTools.R")
+library("meta")
+library("metafor")
+library("survival")
+library("survminer")
+
+# i=match("ENSG00000213754.2",rownames(input))
+# i=match("ENSG00000231246.1",rownames(input))
+# i=grep("ENSG00000280054",rownames(input))
+# i=grep("ENSG00000243384",rownames(input))
+# i=grep("ENSG00000274370",rownames(input))
+# i=grep("ENSG00000266601",rownames(input))
+load("~/hpc/methylation/Pancancer/RNA-seq/rnaseqdata.pancancer.RData")
+TCGAProjects=c("BLCA","BRCA","CESC","CHOL","COAD","ESCA","GBM","HNSC","KICH","KIRC","KIRP","LIHC","LUAD","LUSC","PAAD","PCPG","PRAD","READ","SARC","STAD","THCA","THYM","UCEC")
+panc<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/panc.txt",head=T)
+phen1=read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/TCGA-clinical-11093.tsv",header = T,sep="\t")
+phen2=read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/File_metadata2.txt",header = T,sep="\t")
+head(phen1)
+head(phen2)
+colnames(rnaseqdata)<-unlist(lapply(strsplit(colnames(rnaseqdata),"/"),function(x) x[2]))
+phen<-data.frame(phen2,phen1[match(phen2$cases.0.case_id,phen1$case_id),])
+phen$file_name=gsub(".gz","",phen$file_name)
+# prepare phenotype information
+phen<-phen[match(colnames(rnaseqdata),phen$file_name),]
+phen$phen4<-id2phen4(phen$cases.0.samples.0.submitter_id)
+phen$phen3<-id2phen3(phen$cases.0.samples.0.submitter_id)
+phen$phen2<-id2bin(phen$cases.0.samples.0.submitter_id)
+phen$pid<-phen$project_id
+head(phen)
+                                    
 OS<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/OverallSurvivalTime.txt",head=T,sep="\t")
 # match survival information
 idx<-which(c(phen$phen2==1))
