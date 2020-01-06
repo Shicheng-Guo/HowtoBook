b3a7c04e0b9b11b4ab3be10047e6b824cb273cac
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-05 19:26:36 -0600

    Create step1.download.sh

---
+++ qcRNAseq/step1.download.sh
@@ -0,0 +1,4 @@
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/datasets/GDS4nnn/GDS4146/soft/GDS4146.soft.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/platforms/GPLnnn/GPL97/annot/GPL97.annot.gz
+wget /wp-content/uploads/2013/08/metadata.txt
+gunzip *.gz
