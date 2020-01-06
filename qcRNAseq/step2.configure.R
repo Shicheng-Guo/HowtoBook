cefe4b6613db393f70b789a26d84394c89c68de8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-05 20:01:33 -0600

    Create step2.configure.R

---
+++ qcRNAseq/step2.configure.R
@@ -0,0 +1,12 @@
+setwd('c:/sci/026rplcl/data/microarray/practice2/')
+
+# only if you don't have these packages already:
+# install.packages("stringr")
+# install.packages("gap")
+# install.packages("sqldf")
+# install.packages("reshape")
+library(stringr) # makes string splitting, indexing, regexes easier - R's base functions are quite bad at this
+library(gap) # best library I've found for qq plots
+library(sqldf) # for me, JOIN & GROUP BY are easier to use than R's match/merge and aggregate
+library(reshape) # for converting between matrices and relational models
+options(stringsAsFactors=FALSE) # so that strings stay as strings in all read.table calls
